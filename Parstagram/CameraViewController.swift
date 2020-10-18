ca522f2893045f850daeae702bd2bcecee665d0e
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-17 21:59:53 -0500

    Fix image rendering problem

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -40,10 +40,13 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         post["author"] = PFUser.current()!
         
         let imageData = imageView.image!.pngData()
-        let file = PFFileObject(data: imageData!) //exclamation point to unwrap it
+        print(imageData!)
+        
+        //let file = PFFileObject(data: imageData!) //exclamation point to unwrap it
+        let file = PFFileObject(name: "image.png", data: imageData!)
         post["image"] = file
         
-        post.saveInBackground { (success, error) in
+        post.saveInBackground{ (success, error) in
             if success {
                 self.dismiss(animated: true, completion: nil)
                 print("saved!")
@@ -75,12 +78,10 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 //        picker.mediaTypes = [kUTTypeImage as String]
         
         
-        
-        
         print("In tap gesture")
         if UIImagePickerController.isSourceTypeAvailable(.camera){
             print("Camera ready")
-            picker.sourceType = .photoLibrary
+            picker.sourceType = .camera
 //            picker.cameraCaptureMode = .photo
 //            picker.cameraDevice = .rear
             //addChild(picker)
--- Podfile
+++ Podfile
@@ -1,12 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '14.0'
 
 target 'Parstagram' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Parstagram
-  pod 'Alamofire', '~> 4.4'
+  pod 'Alamofire'
   pod 'AlamofireImage'
   pod "Parse"
   pod "MessageInputBar"
