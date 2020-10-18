85a89aff63d87c7b2b07af42444433d56b42d861
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-18 01:13:49 -0500

    Add comments dynamically, resize cell for it

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -112,7 +112,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = numberofPosts
         
         query.findObjectsInBackground { (posts, error) in
@@ -127,25 +127,67 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ??  [] //in parenthesis this is an optional; ?? is a nil coalescing operator. if nil, set to right
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ??  []
+        
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            
+            let user = post["author"] as! PFUser
+            cell.userNameLabel.text = user.username
+            cell.captionLabel.text = (post["caption"] as! String)
+            
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.photoView.af_setImage(withURL: url)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //every time the user taps, you'll get called back here
+        
+        //choose a post to add a comment to first
         let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
-        cell.userNameLabel.text = user.username
-        cell.captionLabel.text = (post["caption"] as! String)
+        let comment = PFObject(className: "Comments") //once saved, a comment object will be created (in our db)
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
         
+        post.add(comment, forKey: "comments")
         
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        cell.photoView.af_setImage(withURL: url)
-        
-        return cell
-        
+        post.saveInBackground{ (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
         
     }
 
